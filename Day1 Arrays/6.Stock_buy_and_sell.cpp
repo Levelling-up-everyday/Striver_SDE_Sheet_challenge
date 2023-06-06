b6e28b9ed289e45eccb85bb9c4e147050666fd64
Levelling-up-everyday <106878532+Levelling-up-everyday@users.noreply.github.com>
2023-06-06 20:53:04 +0530

    Update 6.Stock_buy_and_sell.cpp

--- Day1 Arrays/6.Stock_buy_and_sell.cpp
+++ Day1 Arrays/6.Stock_buy_and_sell.cpp
@@ -1,2 +1,14 @@
-//TC:
-//SC:
+//TC:O(n)
+//SC:O(1)
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    int mini=prices[0];
+    int profit=0;
+
+    for(int i=1;i<prices.size();i++){
+        mini=min(prices[i],mini);
+        profit=max(profit,prices[i]-mini);
+    }
+    return profit;
+    // Write your code here.
+}
